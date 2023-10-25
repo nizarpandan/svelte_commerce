e73a07231635b18a27283c8c0233785107b3a737
nizarpandan <40788332+nizarpandan@users.noreply.github.com>
2023-10-25 10:11:52 +0800

    products.ts

--- src/lib/data/products.ts
+++ src/lib/data/products.ts
@@ -7,7 +7,7 @@ export const Products: Product[]= [
     color: "Black", 
     price: 35, 
     imageUrl: "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg", 
-    imageDetailUrl: "https://tailwindui.com/img/ecommerce-images/product-page-02-featured-product-shot.jpg"
+    imageDetailUrl: "https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-01.jpg"
   },
   {
     id: "02",
@@ -15,7 +15,7 @@ export const Products: Product[]= [
     color: "Aspen White", 
     price: 35, 
     imageUrl: "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-02.jpg", 
-    imageDetailUrl: "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-02.jpg"
+    imageDetailUrl: "https://tailwindui.com/img/ecommerce-images/product-page-02-featured-product-shot.jpg"
   },
   {
     id: "03",
@@ -23,7 +23,7 @@ export const Products: Product[]= [
     color: "Charcoal", 
     price: 35, 
     imageUrl: "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-03.jpg", 
-    imageDetailUrl: "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-03.jpg"
+    imageDetailUrl: "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-02.jpg"
   },
   {
     id: "04",
